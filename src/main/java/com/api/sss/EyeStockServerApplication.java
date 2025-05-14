b8d0354bff53c06de5e3493239e3dd0741ce5b43
happine2s <happine2s@sookmyung.ac.kr>
2025-05-14 12:32:04 +0900

    [chore] Update project structure

--- src/main/java/com/api/sss/EyeStockServerApplication.java
+++ src/main/java/com/api/sss/EyeStockServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SssApplication {
+public class EyeStockServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SssApplication.class, args);
+		SpringApplication.run(EyeStockServerApplication.class, args);
 	}
 
 }
