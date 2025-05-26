cb048ec006dc406669e1a81b03e8227e7d87048b
jihyeon baek <qormoon@sookmyung.ac.kr>
2025-05-26 14:16:36 +0900

    docs: 회원가입 api 스웨거 설명 추가 #7

--- src/main/java/com/api/sss/login/controller/LoginController.java
+++ src/main/java/com/api/sss/login/controller/LoginController.java
@@ -11,6 +11,11 @@ import com.api.sss.config.response.dto.SuccessStatus;
 import com.api.sss.login.dto.BiometricSignupRequest;
 import com.api.sss.login.service.LoginService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -19,6 +24,19 @@ import lombok.RequiredArgsConstructor;
 public class LoginController {
 	private final LoginService loginService;
 
+	@Operation(summary = "지문 로그인 회원가입 API", description = "device-id와 publicKey 저장하는 회원가입 API입니다.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "device-id & public key 저장 성공"),
+		@ApiResponse(
+			responseCode = "400",
+			description = "이미 등록된 기기",
+			content = @Content(mediaType = "application/json", examples = @ExampleObject(value = """
+			{
+			  "code": 400,
+			  "message": "이미 등록된 디바이스입니다."
+			}"""))
+		)
+	})
 	@PostMapping("/biometric-signup")
 	public ResponseEntity<CustomResponse<Void>> biometricSignup(@RequestBody BiometricSignupRequest request) {
 		loginService.signup(request);
--- src/main/java/com/api/sss/login/dto/BiometricSignupRequest.java
+++ src/main/java/com/api/sss/login/dto/BiometricSignupRequest.java
@@ -1,9 +1,13 @@
 package com.api.sss.login.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class BiometricSignupRequest {
+	@Schema(description = "기기 고유 ID", example = "abc123device")
 	private String deviceId;
+
+	@Schema(description = "생성된 biometric 공개키 (Base64)", example = "MIIBIjANBgkqhkiG9...")
 	private String publicKey;
 }
