8d5550b05e92ed4fc16027849822044d7c2e5a27
jihyeon baek <qormoon@sookmyung.ac.kr>
2025-06-04 15:39:59 +0900

    feat: nas 연결 테스트 코드

--- src/main/java/com/api/sss/test/controller/TestController.java
+++ src/main/java/com/api/sss/test/controller/TestController.java
@@ -1,12 +1,19 @@
 package com.api.sss.test.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class TestController {
+    RestTemplate restTemplate = new RestTemplate();
+    String url = "http://203.153.148.56:5050";
+
     @GetMapping("/test")
-    public String healthCheck() {
-        return "Connection successful";
+    public String healthCheck()
+    {
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        return "NAS 연결 성공: " + response.getBody();
     }
 }
