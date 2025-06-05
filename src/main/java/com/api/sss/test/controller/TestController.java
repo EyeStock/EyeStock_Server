e307a0e6e9d3ef4fb6adcb65972ef2ce342f2531
jihyeon baek <qormoon@sookmyung.ac.kr>
2025-06-05 16:37:09 +0900

    chore: NAS ip 삭제

--- src/main/java/com/api/sss/test/controller/TestController.java
+++ src/main/java/com/api/sss/test/controller/TestController.java
@@ -7,13 +7,10 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class TestController {
-    RestTemplate restTemplate = new RestTemplate();
-    String url = "http://203.153.148.56:5050";
 
     @GetMapping("/test")
     public String healthCheck()
     {
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-        return "NAS 연결 성공: " + response.getBody();
+        return "Connection successful";
     }
 }
