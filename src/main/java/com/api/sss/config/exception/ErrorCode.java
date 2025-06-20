c67d2d0b0fcb29fd3acfe6e344ac261f36a3d26f
jihyeon baek <qormoon@sookmyung.ac.kr>
2025-06-20 13:36:37 +0900

    feat: fast api 에러 핸들러 추가 #14

--- src/main/java/com/api/sss/config/exception/ErrorCode.java
+++ src/main/java/com/api/sss/config/exception/ErrorCode.java
@@ -13,7 +13,8 @@ public enum ErrorCode {
     INVALID_SIGNATURE(401, "서명 검증에 실패했습니다."),
     EXPIRED_TOKEN(401, "토큰이 만료되었습니다."),
     INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
-    INVALID_REFRESH_TOKEN(401, "Refresh Token이 유효하지 않습니다.");
+    INVALID_REFRESH_TOKEN(401, "Refresh Token이 유효하지 않습니다."),
+    FASTAPI_COMMUNICATION_ERROR(500, "FastAPI와의 통신 중 오류가 발생했습니다.");
 
 
     private final int code;
