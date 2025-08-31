d64fc1a7283161a902c758e5d1fc759df73f3a30
happine2s <happine2s@sookmyung.ac.kr>
2025-09-01 00:03:17 +0900

    Fix: Jackson 직렬화 에러 해결

--- src/main/java/com/api/sss/model/dto/NewsResponse.java
+++ src/main/java/com/api/sss/model/dto/NewsResponse.java
@@ -1,6 +1,7 @@
 package com.api.sss.model.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -8,6 +9,8 @@ import java.util.List;
 public class NewsResponse {
     private List<Result> results;
 
+    @Getter
+    @NoArgsConstructor
     public static class Result {
         private String keyword;
         private String url;
