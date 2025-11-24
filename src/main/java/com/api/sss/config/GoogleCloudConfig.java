9909658a1183ce4756d183868867c54f027611f3
sujeongmoon <msjk9000@gmail.com>
2025-11-25 02:47:57 +0900

    feat:#24 STT 사용하여 코인종목명 입력 구현

--- src/main/java/com/api/sss/config/GoogleCloudConfig.java
+++ src/main/java/com/api/sss/config/GoogleCloudConfig.java
@@ -7,21 +7,18 @@ import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.speech.v1.SpeechClient;
 import com.google.cloud.speech.v1.SpeechSettings;
-import com.google.cloud.speech.v1.stub.SpeechStubSettings;
 
 @Configuration
 public class GoogleCloudConfig {
 
-	// encoded-key (base64 문자열) 그대로 주입
 	private final GoogleCredentials googleCredentials;
 
 	public GoogleCloudConfig(
-		@Value("${spring.cloud.gcp.credentials.encoded-key:}")
+		@Value("${spring.cloud.gcp.credentials.encoded-key}")
 		String encodedKey
 	) throws IOException {
 		byte[] decoded = Base64.getDecoder().decode(encodedKey);
--- src/main/java/com/api/sss/config/exception/ErrorCode.java
+++ src/main/java/com/api/sss/config/exception/ErrorCode.java
@@ -6,19 +6,19 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    TEST_ERROR_CODE(400, "응답 테스트 실패입니다."),
-    DEVICE_ALREADY_REGISTERED(400, "이미 등록된 디바이스입니다."),
-    MEMBER_NOT_FOUND(404, "해당 사용자를 찾을 수 없습니다."),
-    INVALID_CHALLENGE(401, "challenge 값이 유효하지 않습니다."),
-    INVALID_SIGNATURE(401, "서명 검증에 실패했습니다."),
-    EXPIRED_TOKEN(401, "토큰이 만료되었습니다."),
-    INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
-    INVALID_REFRESH_TOKEN(401, "Refresh Token이 유효하지 않습니다."),
-    UNSUPPORTED_TOKEN(401,"지원하지 않는 토큰입니다."),
-    FASTAPI_COMMUNICATION_ERROR(500, "FastAPI와의 통신 중 오류가 발생했습니다."),
+	TEST_ERROR_CODE(400, "응답 테스트 실패입니다."),
+	DEVICE_ALREADY_REGISTERED(400, "이미 등록된 디바이스입니다."),
+	MEMBER_NOT_FOUND(404, "해당 사용자를 찾을 수 없습니다."),
+	INVALID_CHALLENGE(401, "challenge 값이 유효하지 않습니다."),
+	INVALID_SIGNATURE(401, "서명 검증에 실패했습니다."),
+	EXPIRED_TOKEN(401, "토큰이 만료되었습니다."),
+	INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
+	INVALID_REFRESH_TOKEN(401, "Refresh Token이 유효하지 않습니다."),
+	UNSUPPORTED_TOKEN(401, "지원하지 않는 토큰입니다."),
+	FASTAPI_COMMUNICATION_ERROR(500, "FastAPI와의 통신 중 오류가 발생했습니다."),
+	FILE_NOT_FOUND(400, "입력 파일이 없습니다."),
+	FILE_READ_ERROR(500, "서버에서 파일을 읽어들이는 중 오류가 발생했습니다.");
 
-    ;
-
-    private final int code;
-    private final String message;
+	private final int code;
+	private final String message;
 }
